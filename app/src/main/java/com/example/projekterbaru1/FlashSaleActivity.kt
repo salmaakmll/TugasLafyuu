e5c6882d11cfd05a1d73c6e512952251baf5bc6f
salmaakmll <salmakamilaarsipkuliah@gmail.com>
2024-05-27 16:24:29 +0700

    tambah flash saleee detail

---
+++ app/src/main/java/com/example/projekterbaru1/FlashSaleActivity.kt
@@ -0,0 +1,28 @@
+package com.example.projekterbaru1
+
+import android.os.Bundle
+import android.widget.ImageView
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class FlashSaleActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_flash_sale)
+
+        // Set padding to avoid system bars overlap
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        // Set onClickListener for the ImageView with id "left"
+        findViewById<ImageView>(R.id.left).setOnClickListener {
+            finish() // This will close the current activity and return to the previous one
+        }
+    }
+}
